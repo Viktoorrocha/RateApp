03bb9b1182a481fe446c7d6796b13946029bada0
Viktor Rocha <viktoorrocha@gmail.com>
2018-11-11 19:22:44 -0400

    Inital

---
+++ app/src/main/java/viktorrocha/br/com/rateapp/MainActivity.java
@@ -0,0 +1,13 @@
+package viktorrocha.br.com.rateapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
